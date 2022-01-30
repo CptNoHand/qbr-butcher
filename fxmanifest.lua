1e8059b1d6aaa79062daeee83b36ab5b2758c642
Sporny <janheidolph@yahoo.de>
2022-01-30 09:36:22 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+game "rdr3"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author "QBR-Sporny"
+version "1.0.0"
+
+client_scripts {
+	'client/main.lua'
+}
+server_scripts {
+	'server/main.lua'
+}
+shared_scripts {
+	'config.lua'
+}
