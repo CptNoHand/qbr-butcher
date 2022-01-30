e5fd31956d38b140c5e449c6c430a0f56be89643
Sporny <janheidolph@yahoo.de>
2022-01-30 12:00:55 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,20 @@
---
+local QBCore = exports['qbr-core']:GetCoreObject()
+
+RegisterServerEvent("qbr-butcher:server:giveitem")
+AddEventHandler("qbr-butcher:server:giveitem", function(item, amount)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(source)
+
+	Player.Functions.AddItem(item, 1)
+    TriggerClientEvent("inventory:client:ItemBox", src, QBCore.Shared.Items[item], "add")
+end)
+
+RegisterServerEvent("qbr-butcher:server:reward")
+AddEventHandler("qbr-butcher:server:reward", function(amount, xp)
+	local _source = source
+	local _amount = tonumber(string.format("%.2f", amount))
+	local Player = QBCore.Functions.GetPlayer(source)
+	
+	Player.Functions.AddMoney("cash", _amount, "butcher-sellmeat")
+	Player.Functions.AddXp("hunting", xp)
+end)
